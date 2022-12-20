3b37afdee2048d32841e48718db445fcfcf1de1b
Sourav Kumar <74768346+sourav-harsh@users.noreply.github.com>
2022-12-20 10:04:42 +0530

    new server

--- config/database.js
+++ config/database.js
@@ -9,7 +9,7 @@ module.exports = ({ env }) => ({
       user: env('DATABASE_USERNAME', 'postgres'),
       password: env('DATABASE_PASSWORD', '29012001Sourav@'),
       schema: env('DATABASE_SCHEMA', 'public'), // Not required
-      ssl: { rejectUnauthorized: false },
+      // ssl: { rejectUnauthorized: false },
     },
   },
 });
